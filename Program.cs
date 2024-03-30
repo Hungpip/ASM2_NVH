b381eeb7bff637d88366ce62d7ed9314131f6de0
HuyMasterTX <huymastertx88@gmail.com>
2024-03-30 15:30:39 +0700

    delete comment

--- Program.cs
+++ Program.cs
@@ -2,9 +2,6 @@ namespace ASM2_NVH {
 
 	internal static class Program {
 
-		/// <summary>
-		///  The main entry point for the application.
-		/// </summary>
 		[STAThread]
 		private static void Main() {
 			ApplicationConfiguration.Initialize();
