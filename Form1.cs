1e9a07a27293af0811d05df474a141198eff28cc
HuyMasterTX <huymastertx88@gmail.com>
2024-03-31 12:59:42 +0700

    re align Form1

--- Form1.cs
+++ Form1.cs
@@ -1,47 +1,35 @@
-namespace ASM2_NVH
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-        string Email = "thanhtoandiennuoc";
-        string password = "123456";
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (KiemTraDangNhap(textBox1.Text, textBox2.Text))
-            {
-                Form2 f = new Form2();
-                f.ShowDialog();
-                
+namespace ASM2_NVH {
 
-            }
-            else
-            {
-                MessageBox.Show("Please retun login", "Waring", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
-        bool KiemTraDangNhap(string Email, string password)
-        {
-            if (Email == this.Email && password == this.password)
-            {
+	public partial class Form1 : Form {
 
-                return true;
-            }
+		public Form1() {
+			InitializeComponent();
+		}
 
-            return false;
-        }
+		private string Email = "thanhtoandiennuoc";
+		private string password = "123456";
 
+		private void button1_Click(object sender, EventArgs e) {
+			if (KiemTraDangNhap(textBox1.Text, textBox2.Text)) {
+				Form2 f = new();
+				f.ShowDialog();
+			} else {
+				MessageBox.Show("Please retun login", "Waring", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+		private bool KiemTraDangNhap(string Email, string password) {
+			if (Email == this.Email && password == this.password) {
+				return true;
+			}
 
-        }
+			return false;
+		}
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            
-        }
-    }
+		private void Form1_Load(object sender, EventArgs e) {
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+		}
+	}
 }
